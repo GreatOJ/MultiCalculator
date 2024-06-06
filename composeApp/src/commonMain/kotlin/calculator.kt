d731651f87043eeb77d3659a02df6d9aa2e2f5e3
GreatOJ <beuchechi8@gmail.com>
2024-06-06 03:24:54 +0100

    Initial commit

---
+++ composeApp/src/commonMain/kotlin/calculator.kt
@@ -0,0 +1,15 @@
+class calculator {
+fun add(left: Int, right: Int): Int {
+    return left.plus(right)
+}
+fun substract(left: Int, right: Int): Int {
+    return left.minus(right)
+}
+fun divide (left :Int, right: Int): Int{
+    return left.div(right)
+}
+fun multiply(left:Int,right:Int ): Int {
+    return left * right
+}
+
+}
